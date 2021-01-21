c59161eb751e315ab2286e5ed25489ba475c4dc1
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-01-21 21:52:09 +0800

    Export fn from only-subtract.js module using `module.exports`

--- calculate.js
+++ calculate.js
@@ -1,5 +1,14 @@
+// math.js
+// `exports.add = ...`
 const math = require("./math");
 console.log(math); // { add: [Function] }
 
 const sum = math.add(1, 2);
 console.log(sum); // 3
+
+// only-subtract.js
+// `module.exports = ...`
+const subtract = require("./only-subtract");
+
+const deduction = subtract(10, 5);
+console.log(deduction); // 5
